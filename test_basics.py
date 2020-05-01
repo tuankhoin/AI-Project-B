007fa842ebbc24b5f8cdaf8fd699655f81062317
tuankhoin <tuankhoin@student.unimelb.edu.au>
2020-05-01 16:27:43 +1000

    update test script

--- test_basics.py
+++ test_basics.py
@@ -1,16 +1,21 @@
 """Run this script to test the newly implemented functions"""
-from player import ExamplePlayer as Player
+from boomers.player import ExamplePlayer as Player
 from utils.update import update_move, update_boom
 
 player = Player('white')
 
 boom = ('BOOM', (0,0))
-move = ('MOVE', 1, (7,0), (7,1))
+move1 = ('MOVE', 1, (7,0), (7,1))
+move2 = ('MOVE', 1, (7,1), (7,0))
 
-update_move(player, 'white', move)
+update_move(player, 'white', move1)
 
 print(player)
 
 update_boom(player, 'white', boom)
+
+print(player)
+
+update_move(player, 'white', move2)
 
 print(player)
