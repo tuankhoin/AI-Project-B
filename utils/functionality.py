4b6ed86044a8169b4824e85e88d13481543bfb13
tuankhoin <tuankhoin@student.unimelb.edu.au>
2020-05-01 21:24:38 +1000

    add comments

--- utils/functionality.py
+++ utils/functionality.py
@@ -2,19 +2,24 @@ def get_available_action(player):
     """Returns a list that contains the available moves of a player"""
     action_list = []
     stack_list = player.player + player.opponent
+    # Making a list of occupied coordinates
     for stack in stack_list:
         stack = (stack[1],stack[2])
 
+    # For each token on board:
     for token in player.player:
         action_list.append(('BOOM', (token[1],token[2])))
 
+        # Check for all available horizontal moves
         for i in range(token[1]-token[0], token[1]+token[0]+1):
             if i == token[1] or i<0 or i>7:
                 continue
             if (i,token[2]) not in stack_list:
+                # All available moves in range
                 for j in range(1, token[0]+1):
                     action_list.append(('MOVE', j, (token[1],token[2]), (i,token[2])))
 
+        # Check for all available vertical moves
         for i in range(token[2]-token[0], token[2]+token[0]+1):
             if i == token[2] or i<0 or i>7:
                 continue
