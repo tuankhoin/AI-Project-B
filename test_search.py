499b54972f2ea84c92f3790b91997c8f3c32eced
tuankhoin <tuankhoin@student.unimelb.edu.au>
2020-05-03 17:03:52 +1000

    Update test_search.py

--- test_search.py
+++ test_search.py
@@ -5,13 +5,15 @@ node = s.Node(None,None,'white')
 
 print(node)
 
-node.expand_all()
+#node.expand_all()
 
 print(len(node.children))
 
 child_node = node.expand(('MOVE', 1, (0,0), (0,1)))
+print(node.player.player)
 boom_node = node.expand(('BOOM', (1,1)))
 
-print(child_node)
+"""print(child_node)
 print(len(node.children))
-print(node.actions)
+print(node.actions)"""
+print(node.player.player)
